84e27f74928c3e5aa88ebdf9703052e3ef90918f
WesleyRodrigues55 <wesley.santos140300@gmail.com>
2023-04-11 22:12:16 -0300

    Criação VIEW tipo_profissional e suas interfaces para inserção e lista

---
+++ SerMais/Repositorio/ITipoProfissionalRepositorio.cs
@@ -0,0 +1,11 @@
+﻿using SerMais.Models;
+
+namespace SerMais.Repositorio
+{
+    public interface ITipoProfissionalRepositorio
+    {
+        void Atualizar(TipoProfissionalModel tipo_profissional);
+        TipoProfissionalModel ObterPorId(int id);
+        List<TipoProfissionalModel> BuscarTodos();
+    }
+}
---
+++ SerMais/Repositorio/TipoProfissionalRepositorio.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SerMais.Data;
+using SerMais.Models;
+
+namespace SerMais.Repositorio
+{
+    public class TipoProfissionalRepositorio : ITipoProfissionalRepositorio
+    {
+        private readonly BancoContext _bancoContext;
+
+        public TipoProfissionalRepositorio(BancoContext bancoContext)
+        {
+            _bancoContext = bancoContext;
+        }
+
+        public void Atualizar(TipoProfissionalModel tipo_profissional)
+        {
+            _bancoContext.Entry(tipo_profissional).State = EntityState.Modified;
+            _bancoContext.SaveChanges();
+        }
+        public TipoProfissionalModel ObterPorId(int id)
+        {
+            return _bancoContext.TIPO_PROFISSIONAL.FirstOrDefault(p => p.ID == id);
+        }
+
+        public List<TipoProfissionalModel> BuscarTodos()
+        {
+            return _bancoContext.TIPO_PROFISSIONAL.ToList();
+        }
+    }
+}
