cbdb88c6e560aa87c7f9f400b73864f1808a01cc
WesleyRodrigues55 <wesley.santos140300@gmail.com>
2023-04-18 21:08:24 -0300

    Criação da página dos profissionasi e do seu controller Alterando a estrutura e cor da Navegação

---
+++ SerMais/Controllers/ProfissionaisController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SerMais.Controllers
+{
+    public class ProfissionaisController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
